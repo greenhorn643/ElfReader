9f7ce6ba80376e0061f8d9f03ad5cb7d3a6fe1da
greenhorn643 <alex.palmer643@gmail.com>
2024-12-02 09:55:50 +1300

    implemented read for headers

--- src/ElfReader.jl
+++ src/ElfReader.jl
@@ -1,5 +1,27 @@
 module ElfReader
 
-# Write your package code here.
+export read_headers
+
+using Transducers
+
+const Headers = Dict{String, String}
+
+struct ElfFileContents
+    headers::Headers
+end
+
+function read_headers(filepath::AbstractString)::Headers
+    (readlines(`readelf -h $filepath`)
+    |> Drop(1)
+    |> Map(parse_header)
+    |> Headers)
+end
+
+function parse_header(line::AbstractString)
+    (split(line, ':')
+    |> Map(strip)
+    |> collect
+    |> h -> (h[1] => h[2]))
+end
 
 end
